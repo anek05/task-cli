1dfab06b0c4ab834aee2af7bcce4234e10f86d1b
anek05 <andreas.eklund94@gmail.com>
2025-03-05 02:00:37 +0100

    Added basic command arguments and logic structure. Also added tasks.json

--- task-cli.py
+++ task-cli.py
@@ -1,2 +1,26 @@
-print("test")
-#https://claude.ai/chat/dca856b3-35bf-4fd1-9e21-819c9029ff34
+#https://roadmap.sh/projects/task-tracker
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="Testing argparse")
+subparsers = parser.add_subparsers(dest="command", help="Avaliable commands")
+
+add_parser = subparsers.add_parser("add", help="Add a task")
+add_parser.add_argument("name", type=str, help="The name of the task")
+
+update_parser = subparsers.add_parser("update", help="Update a task")
+update_parser.add_argument("name", type=str, help="Name of task you want to update")
+
+delete_parser = subparsers.add_parser("delete", help="Delete a task")
+delete_parser.add_argument("name", type=str, help="Name of the task you want to delete")
+
+parser.add_argument("-v", "--verbose", action="store_true", help="Activate verbose mode")
+
+args = parser.parse_args()
+
+if args.command == "add":
+    print(f"added a task, {args.name}")
+if args.command == "update":
+    print(f"updated a task, {args.name}")
+if args.command == "delete":
+    print(f"deletet a task, {args.name}")
