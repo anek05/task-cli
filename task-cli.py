0dabe264ea26a9e981031dca8c27ef9478847ecf
anek05 <andreas.eklund94@gmail.com>
2025-03-06 14:52:48 +0100

    Created Task class

--- task-cli.py
+++ task-cli.py
@@ -4,12 +4,12 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Testing argparse")
 subparsers = parser.add_subparsers(dest="command", help="Avaliable commands")
-
+#argument parser setup
 add_parser = subparsers.add_parser("add", help="Add a task")
 add_parser.add_argument("name", type=str, help="The name of the task")
 
 update_parser = subparsers.add_parser("update", help="Update a task")
-update_parser.add_argument("name", type=str, help="Name of task you want to update")
+update_parser.add_argument("name", type=str, help="Name of the task you want to update")
 
 delete_parser = subparsers.add_parser("delete", help="Delete a task")
 delete_parser.add_argument("name", type=str, help="Name of the task you want to delete")
@@ -17,6 +17,7 @@ delete_parser.add_argument("name", type=str, help="Name of the task you want to 
 parser.add_argument("-v", "--verbose", action="store_true", help="Activate verbose mode")
 
 args = parser.parse_args()
+#############################################################################################
 
 if args.command == "add":
     print(f"added a task, {args.name}")
