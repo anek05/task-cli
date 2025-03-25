62e9f0d39791aad82092028a305a042b59ded3ba
anek05 <andreas.eklund94@gmail.com>
2025-03-25 23:40:17 +0100

    Feature: can now update status of tasks

--- task-cli.py
+++ task-cli.py
@@ -29,6 +29,21 @@ update_parser.add_argument("description", type=str, help="The description of the
 delete_parser = subparsers.add_parser("delete", help="Delete a task")
 delete_parser.add_argument("id", type=int, help="ID of the task you want to delete")
 
+list_parser = subparsers.add_parser("list", help="List all tasks")
+list_parser.add_argument("-a", "--all", action="store_true", help="Show all tasks")
+list_parser.add_argument("-t", "--todo", action="store_true", help="Show todo tasks")
+list_parser.add_argument("-i", "--in-progress", action="store_true", help="Show in progress tasks")
+list_parser.add_argument("-d", "--done", action="store_true", help="Show done tasks")
+
+todo_parser = subparsers.add_parser("mark-todo", help="Change task status to todo")
+todo_parser.add_argument("id", type=int, help="ID of the task you want to change to todo")
+
+in_progress_parser = subparsers.add_parser("mark-in-progress", help="Change task status to in progress")
+in_progress_parser.add_argument("id", type=int, help="ID of the task you want to change to in progress")
+
+done_parser = subparsers.add_parser("mark-done", help="Change task status to done")
+done_parser.add_argument("id", type=int, help="ID of the task you want to change to done")
+
 parser.add_argument("-v", "--verbose", action="store_true", help="Activate verbose mode")
 args = parser.parse_args()
 #############################################################################################
@@ -63,3 +78,38 @@ if args.command == "delete":
         json.dump(task_list, file, indent=4)
     print(f"deleted a task, {task_to_delete['description']}")
 
+if args.command == "list":
+    if args.all:
+        for task in task_list:
+            print(task)
+    elif args.todo:
+        for task in task_list:
+            if task["status"] == "todo":
+                print(task)
+    elif args.in_progress:
+        for task in task_list:
+            if task["status"] == "in progress":
+                print(task)
+    elif args.done:
+        for task in task_list:
+            if task["status"] == "done":
+                print(task)
+
+if args.command == "mark-todo":
+    task_list[args.id-1]["status"] = "todo"
+    with open("tasks.json", "w") as file:
+        json.dump(task_list, file, indent=4)
+    print(f"Changed task {args.id} to todo")
+    
+if args.command == "mark-in-progress":
+    task_list[args.id-1]["status"] = "in progress"
+    with open("tasks.json", "w") as file:
+        json.dump(task_list, file, indent=4)
+    print(f"Changed task {args.id} to in progress")
+    
+if args.command == "mark-done":
+    task_list[args.id-1]["status"] = "done"
+    with open("tasks.json", "w") as file:
+        json.dump(task_list, file, indent=4)
+    print(f"Changed task {args.id} to done")
+    
